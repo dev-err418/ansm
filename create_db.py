38c5a33773763425cab0a66770deeb0fb075d3ca
arthur <arthur@arthurs-mbp.home>
2024-09-04 16:33:09 +0200

    global path

--- create_db.py
+++ create_db.py
@@ -15,7 +15,7 @@ def read_json(file_path: str):
     with open(file_path, "r") as f:
         return json.load(f)
 
-def vectorize_and_append(to_embed_dense: str, to_embed_sparse: str, content: str):
+def vectorize_and_append(to_embed_dense: str, to_embed_sparse: str, payload: Dict):
     dense_vector = create_dense_embedding(to_embed_dense)
     cols, weights = create_sparse_embedding(to_embed_sparse)
     sparse_vector = {
@@ -27,12 +27,10 @@ def vectorize_and_append(to_embed_dense: str, to_embed_sparse: str, content: str
         collection_name=collection_name,
         dense_vector=dense_vector,
         sparse_vector=sparse_vector,
-        payload={
-            "content": content
-        }
+        payload=payload
     )
 
-def recursive_sub_or_content(sub: str, branch: Dict):
+def recursive_sub_or_content(sub: str, branch: Dict, path: str):
     if sub == "content": # on a un child et du contenu
         # enlève les "................."
         content = remove_extra_dots(branch["content"])
@@ -41,16 +39,20 @@ def recursive_sub_or_content(sub: str, branch: Dict):
         content = remove_bracketed_content(content)
 
         vectorize_and_append(
-            to_embed_dense=sub, # la str a dense embed (contenu/titre/path/etc...)
-            to_embed_sparse=sub, # la str a sparse embed (contenu/titre/path/etc...)
-            content=content # le contenu affiché dans la DB pour se reperer
+            to_embed_dense=path, # la str a dense embed (contenu/titre/path/etc...)
+            to_embed_sparse=path, # la str a sparse embed (contenu/titre/path/etc...)
+            payload={
+                "content": content,
+                "title": title
+            } # le contenu affiché dans la DB pour se reperer
         )
     elif sub != "": # on a un sous titre
         print(sub)
         for sub_sub in branch[sub]:
             recursive_sub_or_content(
                 sub=sub_sub,
-                branch=branch[sub]
+                branch=branch[sub],
+                path=path + " " + sub
             )
 
 if __name__ == "__main__":
@@ -68,22 +70,26 @@ if __name__ == "__main__":
         for i in infos:
             # transforme "Autres informations (cliquer pour afficher)" en "Autres informations"
             key = remove_parentheses(i)
-            path = title + key
+            path = title + " " + key
             content = infos[i]
 
             vectorize_and_append(
                 to_embed_dense=path, # la str a dense embed (contenu/titre/path/etc...)
                 to_embed_sparse=path, # la str a sparse embed (contenu/titre/path/etc...)
-                content=content # le contenu affiché dans la DB pour se reperer
+                payload={
+                    "content": content,
+                    "path": path
+                } # le contenu affiché dans la DB pour se reperer
             )
 
         for c in caracteristiques:
             # transforme "1. DENOMINATION DU MEDICAMENT" en "DENOMINATION DU MEDICAMENT"
             key = remove_prefix(c) # ?.lower()
-            path = title + key
+            path = title + " " + key
 
             for sub in caracteristiques[c]:
                 recursive_sub_or_content(
                     sub=sub,
-                    branch=caracteristiques[c]
+                    branch=caracteristiques[c],
+                    path=path
                 )
